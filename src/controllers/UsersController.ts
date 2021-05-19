b6e845c0f41fd487efcfb2fb186ed8958b0526ea
Diego Souza <diegosouza@tradecom.com.br>
2021-05-18 21:18:24 -0300

    Finalizando Users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositores/UsersRepository";
+import { UsersService } from "../services/UsersService";
 
 class UsersController {
   async create(request: Request, response: Response) {
     const { email } = request.body;
+    const usersService = new UsersService()
 
-    const usersRepository = getCustomRepository(UsersRepository);
-
-    const user = usersRepository.create({
+    const user = await usersService.create(
       email
-    });
+    );
 
-    await usersRepository.save(user);
     return response.json(user);
   }
 }
