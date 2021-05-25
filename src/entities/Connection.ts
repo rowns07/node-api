76593670164ceeb83f87ee2d3d77c7c18ec06440
Diego Souza <diegosouza@tradecom.com.br>
2021-05-24 23:21:17 -0300

    Create Service, Migration, Entity and Repository Connection

---
+++ src/entities/Connection.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Users } from './Users';
+
+@Entity("connections")
+class Connection {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: String;
+
+  @JoinColumn({ name: "user_id" })
+  @ManyToOne(() => Users)
+  user: Users;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  socket_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+
+}
+
+export { Connection }
--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -4,5 +4,7 @@ import { io } from '../http';
 io.on("connect", (socket: Socket) => {
   socket.on("client_first_access", (params) => {
     console.log(params);
+
+    //salvar a conexao com o socket_idm user_id
   })
 });
